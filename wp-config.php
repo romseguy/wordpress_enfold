9597da6e4082d58baf631fe804955fa0ea088ee4
Romain Séguy <rom.seguy@gmail.com>
2019-12-25 23:38:15 +0100

    1

--- wp-config.php
+++ wp-config.php
@@ -18,9 +18,9 @@
  */
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'occitanie');
+define('DB_NAME', 'fresh');
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'occitanie');
+define('DB_USER', 'fresh');
 /** Mot de passe de la base de données MySQL. */
 define('DB_PASSWORD', 'wxcv');
 /** Adresse de l’hébergement MySQL. */
